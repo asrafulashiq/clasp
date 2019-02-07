99ac01627ea41ab2c61ea37ae729b54035091a2b
asrafulashiq <asrafulashiq@gmail.com>
2019-02-07 00:42:53 -0500

    lfs

---
+++ detection_test/utils.py
@@ -0,0 +1,40 @@
+""" utility functions """
+import pathlib
+from matplotlib import pyplot as plt
+import cv2
+import matplotlib
+
+
+def plot_cv(image, axes=None, show=True, fig_number=None):
+    """ plot cv2 images  """
+    if axes is None:
+        axes = plt.axes()
+    if len(image.shape) == 3:
+        axes.imshow(image[:, :, ::-1])
+    else:
+        axes.imshow(image)
+    if show:
+        matplotlib.use('Qt5Agg')
+        if not fig_number is None:
+            plt.figure(int(fig_number))
+        plt.show()
+    return axes
+
+
+def get_frame_list(src_dir, skip_init=0, num_files=None, skip_end=0, delta=1):
+    """get all frames from a directory"""
+    if not isinstance(src_dir, pathlib.PurePath):
+        src_dir = pathlib.Path(src_dir)
+    files_in_dir = sorted(list(src_dir.iterdir()))
+    if not num_files is None:
+        indices = slice(skip_init, skip_init+delta*num_files, delta)
+    else:
+        indices = slice(skip_init, len(files_in_dir)-skip_end, delta)
+    return files_in_dir[indices]
+
+
+def get_images_from_dir(src_dir, skip_init=0, skip_end=0, num_files=None, delta=1):
+    """ get images as numpy array from a folder"""
+    for imfile in get_frame_list(src_dir, skip_init, num_files, skip_end, delta):
+        image = cv2.imread(str(imfile))
+        yield image, imfile
---
+++ labelling/extract_frames.py
@@ -0,0 +1,35 @@
+from pathlib2 import Path
+import os
+import shutil
+
+HOME_DIR = Path('/media/ash/New Volume/shared_folder/all_videos/')
+OUT_DIR = Path('./data')
+OUT_DIR.mkdir(parents=True, exist_ok=True)
+
+
+def sample_from_folder(folder_name, out_folder, delta=30,
+                       skip_initial=0, skip_final=0):
+    list_ims = sorted(list(folder_name.iterdir()))
+
+
+    for i in range(skip_initial, len(list_ims)-skip_final, delta):
+        imfile = list_ims[i]
+        file_write = OUT_DIR / os.path.sep.join(imfile.parts[-3:-1])
+        file_write.mkdir(parents=True, exist_ok=True)
+        shutil.copy2(str(imfile), str(file_write))
+
+
+if __name__ == "__main__":
+    video_parents = ['9A']
+    camera_names = ['9']
+
+    for vid in video_parents:
+        for cam in camera_names:
+            src_folder = HOME_DIR / vid / cam
+            assert src_folder.exists()
+            sample_from_folder(
+                src_folder, OUT_DIR,
+                delta = 50,
+                skip_initial=300,
+                skip_final=2000,
+            )
