4d6281656b660b64315fb9a61e98cadaaf5b2ee6
asrafulashiq <asrafulashiq@gmail.com>
2019-04-17 15:23:25 -0400

    recoverd some corrupted files in labelling

---
+++ labelling/sloth_config.py
@@ -0,0 +1,52 @@
+from PyQt4.QtGui import QPen
+from PyQt4.Qt import Qt
+from sloth.items import RectItem
+
+
+class CustomRectItem(RectItem):
+    def __init__(self, *args, **kwargs):
+        RectItem.__init__(self, *args, **kwargs)
+
+        # set drawing pen to red with width 2
+        self.setPen(QPen(Qt.red, 2))
+
+
+LABELS = (
+    {
+        'attributes': {
+            'class':      'Person',
+        },
+        'inserter': 'sloth.items.RectItemInserter',
+        'item':     CustomRectItem,
+        'hotkey':   'p',
+        'text':     'Person',
+    },
+    {
+        'attributes': {
+            'class':      'binEMPTY',
+        },
+        'inserter': 'sloth.items.RectItemInserter',
+        'item':     CustomRectItem,
+        'hotkey':   'b',
+        'text':     'binEMPTY',
+    },
+    {
+        'attributes': {
+            'class':      'binFULL',
+        },
+        'inserter': 'sloth.items.RectItemInserter',
+        'item':     CustomRectItem,
+        'hotkey':   'f',
+        'text':     'binFULL',
+    },
+    {
+        'attributes': {
+            'class':      'dvi',
+        },
+        'inserter': 'sloth.items.RectItemInserter',
+        'item':     CustomRectItem,
+        'hotkey':   'd',
+        'text':     'dvi',
+    }
+
+)
