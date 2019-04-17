36e01e09d9d26f72dfb678dff888c2205f8ea3c6
asrafulashiq <asrafulashiq@gmail.com>
2019-04-17 12:32:47 -0400

    added detectron

---
+++ detectron/run_infer.sh
@@ -0,0 +1,6 @@
+python tools/infer_simple.py \
+    --cfg configs/my_cfg/e2e_faster_rcnn_R-50-C4_1x.yaml \
+    --output-dir /home/ash/Desktop/clasp_detection/output/detectron-output \
+    --image-ext jpg \
+    --wts /home/ash/Desktop/clasp_detection/weights/detection-weights/train/clasp_detect/generalized_rcnn/model_final.pkl \
+    /home/ash/Desktop/images_track_clasp/tmp
