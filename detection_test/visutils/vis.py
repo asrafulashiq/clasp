36d5a57212cb18290167e26f450ce3caca2cf57f
asrafulashiq <asrafulashiq@gmail.com>
2019-09-27 18:30:07 -0400

    /

--- detection_test/visutils/vis.py
+++ detection_test/visutils/vis.py
@@ -8,38 +8,58 @@ _RED = (255, 10, 10)
 _BLUE = (10, 10, 255)
 
 
-class_to_color = {
-    'passengers': _BLUE,
-    'items': _GREEN,
-    'other': _RED
-}
+class_to_color = {"passengers": _BLUE, "items": _GREEN, "other": _RED}
 
-def vis_class_label(img, pos, class_str, label, color=_RED, font_scale=0.5, thickness=1):
+
+def vis_class_label(
+    img, pos, class_str, label, color=_RED, font_scale=0.5, thickness=1
+):
     """Visualizes the class."""
     img = img.astype(np.uint8)
     x0, y0 = int(pos[0]), int(pos[1])
     x1, y1 = int(pos[2]), int(pos[3])
     # Compute text size.
-    txt = class_str
     font = cv2.FONT_HERSHEY_SIMPLEX
-    ((txt_w, txt_h), _) = cv2.getTextSize(txt, font, font_scale, 1)
-    # Place text background.
-    back_tl = x0, y0 - int(1.3 * txt_h)
-    back_br = x0 + txt_w, y0
+    if len(class_str) != 0:
+        txt = class_str
+        ((txt_w, txt_h), _) = cv2.getTextSize(txt, font, font_scale, thickness)
+        # Place text background.
+        back_tl = x0 , y0 - int(1.5 * txt_h)
+        back_br = x0 + int(1.5 * txt_w), y0
 
-    cv2.rectangle(img, back_tl, back_br, color, thickness=thickness)
-    # Show text.
-    txt_tl = x0, y0 - int(0.3 * txt_h)
-    cv2.putText(img, txt, txt_tl, font, font_scale,
-                color, lineType=cv2.LINE_AA, thickness=thickness)
+        cv2.rectangle(img, back_tl, back_br, color, thickness=thickness)
+        # Show text.
+        txt_tl = x0 + int(0.05 * txt_w) , y0 - int(0.3 * txt_h)
+        cv2.putText(
+            img,
+            txt,
+            txt_tl,
+            font,
+            font_scale,
+            color,
+            lineType=cv2.LINE_AA,
+            thickness=thickness,
+        )
 
     # put label
     if label:
-        ((lbl_w, lbl_h), _) = cv2.getTextSize(str(label), font, 3*font_scale, 5)
-        lbl_tl = int((x0+x1)/2) - int(0.3 * lbl_w), int((y0+y1)/2) - \
-                    int(0 * lbl_h)
-        cv2.putText(img, str(label), lbl_tl, font, 1.5* font_scale,
-                    color, thickness=2*thickness, lineType=cv2.LINE_AA)
+        ((lbl_w, lbl_h), _) = cv2.getTextSize(
+            str(label), font, 1.2 * font_scale, thickness
+        )
+        lbl_tl = (
+            int((x0 + x1) / 2) - int(0.3 * lbl_w),
+            int((y0 + y1) / 2) - int(0 * lbl_h),
+        )
+        cv2.putText(
+            img,
+            str(label),
+            lbl_tl,
+            font,
+            1.2 * font_scale,
+            color,
+            thickness=thickness,
+            lineType=cv2.LINE_AA,
+        )
 
     return img
 
@@ -55,18 +75,28 @@ def vis_bbox(img, bbox, class_str=None, thick=2, alpha=0.1, color=_RED):
     cv2.rectangle(img, (x0, y0), (x1, y1), color, thickness=thick)
     cv2.rectangle(overlay, (x0, y0), (x1, y1), color, -1)  # filled rectangle
 
-    cv2.addWeighted(overlay, alpha, img, 1-alpha, 0, img)
+    cv2.addWeighted(overlay, alpha, img, 1 - alpha, 0, img)
 
     return img
 
 
-def vis_bbox_with_str(img, bbox, class_str, label, color=_RED,  thick=2):
+def vis_bbox_with_str(
+    img, bbox, class_str, label, color=_RED, thick=2, font_scale=0.5
+):
     """Visualizes a bounding box."""
     # img = img.astype(np.uint8)
     img = vis_bbox(img, bbox, thick=thick, color=color, alpha=0.15)
 
     # add text
-    img = vis_class_label(img, bbox, class_str, label, color=color, font_scale=0.5, thickness=1)
+    img = vis_class_label(
+        img,
+        bbox,
+        class_str,
+        label,
+        color=color,
+        font_scale=font_scale,
+        thickness=thick,
+    )
 
     return img
 
@@ -86,5 +116,5 @@ def vis_pax(img, pax):
         bbox = person.pos
         label = person.label
         img = vis_bbox(img, bbox, class_str=person.cls)
-        img = vis_class_label(img, bbox, 'passengers', label)
+        img = vis_class_label(img, bbox, "passengers", label)
     return img
