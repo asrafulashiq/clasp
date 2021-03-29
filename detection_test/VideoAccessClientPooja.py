5758d3e0d9f19603264c7a0f5e842e5a9afc2693
asrafulashiq <asrafulashiq@gmail.com>
2021-03-29 18:58:31 -0400

    update

--- detection_test/VideoAccessClientPooja.py
+++ detection_test/VideoAccessClientPooja.py
@@ -6,7 +6,9 @@ import os
 import yaml
 import time
 import psutil
+import argparse
 import tracemalloc
+
 
 def GetFlags(flag_file):
     flags = {}
@@ -14,9 +16,11 @@ def GetFlags(flag_file):
         flags = yaml.safe_load(stream)
     return flags
 
-def WriteFlags(flags,flag_file):
+
+def WriteFlags(flags, flag_file):
     with open(flag_file, 'w') as file:
         yaml.dump(flags, file)
+
 
 def CleanInputDir(inputdir):
     framelist = [f for f in os.listdir(inputdir) if f.endswith(".jpg")]
@@ -24,20 +28,25 @@ def CleanInputDir(inputdir):
         os.remove(os.path.join(inputdir, f))
     return
 
+
 tracemalloc.start()
 current_ram_usage = []
 
 myurl = 'http://127.0.0.1:5000/frames'
 
+parser = argparse.ArgumentParser()
+parser.add_argument("--start_frame", "-s", type=str, default='0.0')
+args = parser.parse_args()
+
 #dataset = str(input("Enter the dataset:"))
 dataset = "exp2training"
 cameralist = '2,4,5,9,11,13,14,20'
-timeoffset = "0.00"
+timeoffset = args.start_frame
 duration = 4.0
 duration = "{0:.2f}".format(float(duration))
 framerate = "10"
 count = 0
-filesize = "720x1080" #or "720x1080"
+filesize = "720x1080"  #or "720x1080"
 
 inputdir = "/data/ALERT-SHARE/alert-api-wrapper-data/"
 Wrapper_Flag_File = "/data/ALERT-SHARE/alert-api-wrapper-data/runtime-files/Flags_Wrapper.yaml"
@@ -47,27 +56,23 @@ MU_Flag_File = "/data/ALERT-SHARE/alert-api-wrapper-data/runtime-files/Flags_MU.
 CleanInputDir(inputdir)
 #Set wrapper flag defaults
 wrapperFlags = {
-  "Frames_Ready_MU": "FALSE",
-  "Frames_Ready_NEU": "FALSE",
-  "Frames_Ready_RPI": "FALSE",
-  "No_More_Frames": "FALSE",
-  "Next_Batch_Requested": "FALSE"
+    "Frames_Ready_MU": "FALSE",
+    "Frames_Ready_NEU": "FALSE",
+    "Frames_Ready_RPI": "FALSE",
+    "No_More_Frames": "FALSE",
+    "Next_Batch_Requested": "FALSE"
 }
 #Set RPI flag defaults
 RPIFlags = {
-  "Bin_Processed": "FALSE",
-  "Batch_Processed": "FALSE",
+    "Bin_Processed": "FALSE",
+    "Batch_Processed": "FALSE",
 }
 
 #Set MU flag defaults
-MUFlags = {
-  "People_Processed": "FALSE"
-}
+MUFlags = {"People_Processed": "FALSE"}
 
 #Set NEU flag defaults
-NEUFlags = {
-  "Association_Ready": "FALSE"
-}
+NEUFlags = {"Association_Ready": "FALSE"}
 
 #Write all flag Defaults
 try:
@@ -105,8 +110,15 @@ while True:
             WriteFlags(wrapperFlags, Wrapper_Flag_File)
         except Exception as ex:
             print(ex)
-        inputParams = {"dataset": dataset, "cameralist": cameralist, "timeoffset": timeoffset, "duration": duration,
-                       "filesize": filesize, "inputdir": inputdir, "framerate": framerate}
+        inputParams = {
+            "dataset": dataset,
+            "cameralist": cameralist,
+            "timeoffset": timeoffset,
+            "duration": duration,
+            "filesize": filesize,
+            "inputdir": inputdir,
+            "framerate": framerate
+        }
         jsoninputParams = json.dumps(inputParams)
         jsonParams = {"APIParams": jsoninputParams}
         response = requests.get(url=myurl, params=jsonParams)
@@ -128,7 +140,9 @@ while True:
         except Exception as ex:
             print(ex)
         current, peak = tracemalloc.get_traced_memory()
-        print(f"Current memory usage is {current / 10 ** 6}MB; Peak was {peak / 10 ** 6}MB")
+        print(
+            f"Current memory usage is {current / 10 ** 6}MB; Peak was {peak / 10 ** 6}MB"
+        )
         current_ram_usage.append(current)
         timeoffset = float(timeoffset) + float(duration)
         timeoffset = round(timeoffset, 2)
@@ -156,10 +170,18 @@ while True:
         if batchIteration != 1:
             overall_batch_elapsed_time = time.time() - overall_batch_start_time
             overall_batch_times.append(overall_batch_elapsed_time)
-            print("Batch Processing Time: " + time.strftime("%H:%M:%S", time.gmtime(overall_batch_elapsed_time)))
+            print("Batch Processing Time: " + time.strftime(
+                "%H:%M:%S", time.gmtime(overall_batch_elapsed_time)))
         overall_batch_start_time = time.time()
-        inputParams = {"dataset": dataset, "cameralist": cameralist, "timeoffset": timeoffset, "duration": duration,
-                           "filesize": filesize, "inputdir": inputdir, "framerate": framerate}
+        inputParams = {
+            "dataset": dataset,
+            "cameralist": cameralist,
+            "timeoffset": timeoffset,
+            "duration": duration,
+            "filesize": filesize,
+            "inputdir": inputdir,
+            "framerate": framerate
+        }
         jsoninputParams = json.dumps(inputParams)
         jsonParams = {"APIParams": jsoninputParams}
         response = requests.get(url=myurl, params=jsonParams)
@@ -181,18 +203,29 @@ while True:
             except Exception as ex:
                 print(ex)
             total_elapsed_time = time.time() - total_start_time
-            print("Total Processing Time: " + time.strftime("%H:%M:%S", time.gmtime(total_elapsed_time)))
-            overall_avg_batch_time = sum(overall_batch_times)/len(overall_batch_times)
-            print("Average Oerall Batch Processing Time: " + time.strftime("%H:%M:%S", time.gmtime(overall_avg_batch_time)))
+            print("Total Processing Time: " +
+                  time.strftime("%H:%M:%S", time.gmtime(total_elapsed_time)))
+            overall_avg_batch_time = sum(overall_batch_times) / len(
+                overall_batch_times)
+            print(
+                "Average Oerall Batch Processing Time: " +
+                time.strftime("%H:%M:%S", time.gmtime(overall_avg_batch_time)))
             overall_max_batch_time = max(overall_batch_times)
-            print("Max Overall Batch Processing Time: " + time.strftime("%H:%M:%S", time.gmtime(overall_max_batch_time)))
-            wrapper_avg_batch_time = sum(wrapper_batch_times)/len(wrapper_batch_times)
-            print("Average Wrapper Batch Processing Time: " + str(wrapper_avg_batch_time))
+            print(
+                "Max Overall Batch Processing Time: " +
+                time.strftime("%H:%M:%S", time.gmtime(overall_max_batch_time)))
+            wrapper_avg_batch_time = sum(wrapper_batch_times) / len(
+                wrapper_batch_times)
+            print("Average Wrapper Batch Processing Time: " +
+                  str(wrapper_avg_batch_time))
             wrapper_max_batch_time = max(wrapper_batch_times)
-            print("Max Wrapper Batch Processing Time: " + str(wrapper_max_batch_time))
+            print("Max Wrapper Batch Processing Time: " +
+                  str(wrapper_max_batch_time))
             current, peak = tracemalloc.get_traced_memory()
             current_ram_usage.append(current)
-            print(f"Average memory usage is {(sum(current_ram_usage) / len(current_ram_usage)) / 10 ** 6}MB; ")
+            print(
+                f"Average memory usage is {(sum(current_ram_usage) / len(current_ram_usage)) / 10 ** 6}MB; "
+            )
             print(f"Peak was {peak / 10 ** 6}MB")
             tracemalloc.stop()
             break
